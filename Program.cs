0080fba237b81c408e86f785be77d988a29b0892
Kubilay Berk ARSLAN <damngineer@gmail.com>
2021-02-13 02:25:10 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            MyDictionary<string> isimler = new MyDictionary<string>();
+
+            isimler.Add("Kubix");
+            isimler.Add("Berkius");
+            isimler.Add("Tivorlu Ismail");
+
+            Console.WriteLine(isimler.Length + " isim bulundu;");
+
+            foreach (var item in isimler.Items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
